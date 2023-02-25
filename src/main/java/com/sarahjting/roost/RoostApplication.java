a4802195eb410b49ff1d454bb7c1e6d3ab499290
lulu <sarah.j.ting@gmail.com>
2023-02-25 09:44:43 +0900

    initial commit

---
+++ src/main/java/com/sarahjting/roost/RoostApplication.java
@@ -0,0 +1,13 @@
+package com.sarahjting.roost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoostApplication.class, args);
+	}
+
+}
