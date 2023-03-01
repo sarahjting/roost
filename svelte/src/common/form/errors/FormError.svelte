0f405f46fb955b7e781e32568e4d0ec0689df1d9
lulu <sarah.j.ting@gmail.com>
2023-03-01 14:05:34 +0900

    add form error components

---
+++ svelte/src/common/form/errors/FormError.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let errors: Array<any> = [];
+</script>
+
+{#if errors.length}
+	<div class="mt-2 text-rose-500">
+		There was a problem with your form. Please re-review the supplied fields.
+	</div>
+{/if}
---
+++ svelte/src/common/form/errors/FormFieldError.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let errors: Array<any> = [];
+	export let field: string;
+</script>
+
+{#if errors.filter((e) => e.field === field).length}
+	<ul class="mt-2 text-rose-500">
+		{#each errors.filter((e) => e.field === field) as error}
+			<li>{error.defaultMessage}</li>
+		{/each}
+	</ul>
+{/if}
