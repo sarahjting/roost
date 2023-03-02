4f4e414a958c83a8887489944e762d6168a4ab03
lulu <sarah.j.ting@gmail.com>
2023-03-02 11:53:53 +0900

    refac: refactor navbar links

---
+++ svelte/src/routes/(app)/LayoutNavbarLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
+	export let title: string;
+	export let icon: ConstructorOfATypedSvelteComponent;
+</script>
+
+<a
+	href="#"
+	class="text-rose-200 hover:bg-rose-900 group flex items-center px-3 py-2 text-sm font-medium rounded-md"
+>
+	<svelte:component
+		this={icon}
+		className="text-rose-300 group-hover:text-rose-200 flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+	/>
+	<span class="truncate group-hover:text-rose-100">{title}</span>
+</a>
